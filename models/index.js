0503d29e40a436b8e2d522d5eaffcaf784f766cc
Stefania <stefinitu@gmail.com>
2024-03-13 11:17:31 +0900

    Connection to AWS DB

--- models/index.js
+++ models/index.js
@@ -3,21 +3,18 @@ const {Sequelize} = require('sequelize');
  const db= {};
  console.log("Here in index, connecting to the db");
 
-// db.sequelize = new Sequelize("postgres://username:pass@url:5432/postgres",{
-// logging:false,
-// maxConcurrentQueries:700,
-// dialect:"postgres",
-// protocol:"postgres",
-// dialectOptions:{
-//     ssl:{
-//         rejectUnauthorized:false
-//     },
-//     connectTimeout:1000
-// }
-//  });
-
-
- db.sequelize = new Sequelize("postgres://postgres:postgres@localhost:5432/postgres",{
+ db.sequelize = new Sequelize("postgres://postgres:postgres@ruverse.cvnpqsvbcz4r.ap-northeast-2.rds.amazonaws.com:5432/postgres",{
+    logging:false,
+    maxConcurrentQueries:800,
+    dialect:"postgres",
+    protocol:"postgres",
+    dialectOptions:{
+        ssl:{
+            rejectUnauthorized:false
+        },
+        connectTimeout:7000
+    }
   });
+  
  db.Models=require("./model")(db.sequelize, Sequelize);
  module.exports={db};
