a2b9ae91207dc2da53dead45787236b1929b0f83
Stefania <stefinitu@gmail.com>
2024-03-13 12:01:03 +0900

    Added Bucket Object

--- controller/archive.controller.js
+++ controller/archive.controller.js
@@ -36,18 +36,18 @@ var Archive=dbContext.db.Models[0];
     })
 
     Archive=dbContext.db.Models[0]; 
-
+    var AWS=require("aws-sdk");
     //UPLOADING FILE TO AWS S3
-    // AWS.config.update({region: 'us-west-2'});
-    // s3 = new AWS.S3();
+    AWS.config.update({region: 'ap-northeast-2'});
+    s3 = new AWS.S3().getObject({Bucket:"ruverse"});
 
-    // s3.listBuckets(function (err, data) {
-    //     if (err) {
-    //       console.log("Error", err);
-    //     } else {
-    //       console.log("Success", data.Buckets);
-    //     }
-    //   });
+    s3.listBuckets(function (err, data) {
+        if (err) {
+          console.log("Error", err);
+        } else {
+          console.log("Success", data.Buckets);
+        }
+      });
 
 //UPLOAD
 
