86d6a48e626a3ebc4d48a705f5d27014e21cc3f0
Stefania <stefinitu@gmail.com>
2024-03-14 12:54:11 +0900

    Changed from http to https

--- routes/archive.routes.js
+++ routes/archive.routes.js
@@ -4,7 +4,6 @@ const router=express.Router();
 
 router.get("/",archiveController.findAll);
 router.post("/",archiveController.create);
-router.get("/create",archiveController.createOne);
 router.delete("/:id", archiveController.delete);
 router.put("/:obj", archiveController.update)
 
