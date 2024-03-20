d883db515141347caa684e29aa7305daf527bade
Stefania <stefinitu@gmail.com>
2024-03-20 19:46:42 +0900

    Changed DB structure

--- server.js
+++ server.js
@@ -44,8 +44,8 @@ db.db.sequelize
 
 app.use("/", archiveRouter);
 
-http.createServer(app).listen(8080,function(req,res){
-    console.log("Server started on port 8080")
+https.createServer(app).listen(443,function(req,res){
+    console.log("Server started on port 443")
 });
 
 // const options={
