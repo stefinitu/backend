966cdaf266f238aa72eea5917eb2dbfe4c4f3d5b
Stefania <stefinitu@gmail.com>
2024-03-20 20:34:22 +0900

    Changed DB structure

--- server.js
+++ server.js
@@ -45,9 +45,6 @@ db.db.sequelize
 app.use("/", archiveRouter);
 
 const options={
-    key:fs.readFileSync("./nodejsssl/private.key"),
-    cert:fs.readFileSync("./nodejsssl/certificate.crt"),
-    ca:fs.readFileSync("./nodejsssl/ca_bundle.crt"),
 }
 https.createServer(options,app).listen(443,function(req,res){
     console.log("Server started on port 443")
