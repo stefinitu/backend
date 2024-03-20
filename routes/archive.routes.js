eb7e5957ac1a38e71deb99d4bab0a677a2d6a6e4
Stefania <stefinitu@gmail.com>
2024-03-20 17:58:27 +0900

    Added update function2

--- routes/archive.routes.js
+++ routes/archive.routes.js
@@ -1,4 +1,6 @@
 const express=require("express");
+const multer=require('multer')
+const upload=multer();
 const archiveController = require("./../controller/archive.controller");
 const router=express.Router();
 
@@ -6,6 +8,6 @@ router.get("/",archiveController.findAll);
 router.post("/",archiveController.create);
 router.delete("/:id", archiveController.delete);
 router.put("/:obj", archiveController.update)
-router.post("/upload",archiveController.uploadFile);
+router.post("/upload",upload.any(),archiveController.uploadFile);
 
 module.exports=router;
