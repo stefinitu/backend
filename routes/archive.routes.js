68b61fdfaddc130844464b7cd1d5fca0cab07731
Stefania <stefinitu@gmail.com>
2024-03-20 23:30:26 +0900

    Changed DB structure

--- routes/archive.routes.js
+++ routes/archive.routes.js
@@ -5,9 +5,9 @@ const archiveController = require("./../controller/archive.controller");
 const router=express.Router();
 
 router.get("/",archiveController.findAll);
-//router.post("/upload",upload.any(), archiveController.create);
+router.post("/upload",upload.any(), archiveController.create);
 router.delete("/:id", archiveController.delete);
 router.put("/:obj", archiveController.update)
-router.post("/upload",upload.any(),archiveController.uploadFile);
+//router.post("/upload",upload.any(),archiveController.uploadFile);
 
 module.exports=router;
